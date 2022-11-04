cd7c408c7177a89be07f512a4d17f99086e5af2a
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-04 13:01:32 -0500

    added password confirmation logic in javascript, but introduced a bug that is slowing down the browser. I am currently using a old intel celeron cpu which may be one reason for the bug. But I think that there is a serious error in the javascript causing the browser to freeze. will investigate

---
+++ app.js
@@ -0,0 +1,32 @@
+
+/*  //add an event listener to the confirm password field and compare to the password field
+document.getElementById('confirm-password').addEventListener('keyup', validate_password);
+ 
+//compare the password and confirm password fields
+function validate_password() {
+     
+        var pass = document.getElementById('password').value;
+        var confirm_pass = document.getElementById('confirm-password').value;
+
+        while (pass != confirm_pass) {
+            if (true) {
+            document.getElementById('message').style.color = 'red';
+            document.getElementById('message').innerHTML
+            = '* Use same password';
+            break;
+            } else {
+            document.getElementById('message').innerHTML = '';
+            }
+        }
+} */
+
+/*             if (pass != confirm_pass) {
+                
+            } else {
+                document.getElementById('message').innerHTML = '';
+            }
+    } */
+
+//remove error message when password and confirm password fields match
+        
+        
